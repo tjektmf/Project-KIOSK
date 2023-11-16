cecc4fa6b718233270bf903647d8bb1ca2fa68b5
hyemi-bang <pbhm1124@gmail.com>
2023-11-16 14:19:01 +0900

    update mainTimer-hm

--- Project1/src/pj/hm/IceCreamShopCover.java
+++ Project1/src/pj/hm/IceCreamShopCover.java
@@ -2,7 +2,7 @@ package pj.hm;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,50 +22,71 @@ import javax.swing.Timer;
 
 public class IceCreamShopCover extends JFrame {
 
+	// img
+	String[] imagePaths = { 
+			"C:/pj/img/ad1.png",
+            "C:/pj/img/ad2.png",
+            "C:/pj/img/ad3.png",
+            "C:/pj/img/ad4.png",
+            "C:/pj/img/베라img.png"};
+	
+	int currentImgIndex = 0;
+	
     public IceCreamShopCover() {
         setTitle("베스킨라빈스");
-        setLayout(new BorderLayout());
 
         // 이미지를 표시할 JLabel
         JLabel mainLabel = new JLabel();
         // 투명한 패널 위에 글씨를 표시할 JLabel
         JLabel textLabel = new JLabel("화면을 클릭하세요");
-        // 이미지 파일 경로
-        String imagePath = "C:/Users/Gi7E-36/Desktop/img/베라img.png"; // 파일 확장자 추가
-        
-        Timer change = new Timer(3000, new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-					
-			}
-			String imagePath2 = "C:/Users/Gi7E-36/Desktop/img/ad1.png";
-        });
-        
-        for(int i = 0 ; i < 5; i++) {
-        	change.start();
-        }
-        change.stop();
-
-        try {
-            BufferedImage image = ImageIO.read(new File(imagePath));
-            Image scaledImage = image.getScaledInstance(540, 960, Image.SCALE_SMOOTH);
-            mainLabel.setIcon(new ImageIcon(scaledImage));
-        } catch (IOException e) {
-        	System.out.println("이미지오류");
-            e.printStackTrace();
-        }
+        textLabel.setFont(new Font("맑은고딕", Font.BOLD, 40));
 
         // 투명한 패널을 만들어 글씨를 포함시키고 위치 설정
-        JPanel textPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
-        textPanel.setSize(540, 960);
+        JPanel textPanel = new JPanel();
+        textPanel.setBounds(150, 200, 400, 400);
         textPanel.setBackground(new Color(0,0,0,0)); // 투명 설정
         add(textPanel);
         textPanel.add(textLabel);
 
+        
+        // 이미지 파일 3초마다 바뀌도록
+        // 이미지 마다 글씨가 들어가야함
+
         // 전체를 감싸는 패널
         JPanel mainPanel = new JPanel(new BorderLayout());
         mainPanel.add(mainLabel, BorderLayout.CENTER);
+        Timer changeImg = new Timer(3000, new ActionListener() {
+        	
+        	   @Override         
+               public void actionPerformed(ActionEvent e) {
+                  String imagePath = imagePaths[currentImgIndex];
+                  updateImg(mainLabel);
+                  mainLabel.add(textPanel);
+                  currentImgIndex = (currentImgIndex + 1 ) % imagePaths.length;
+               }
+        	   
+        	   
+//        	@Override			
+//        	public void actionPerformed(ActionEvent e) {
+//        		String imagePath = imagePaths[currentImgIndex];
+//        		updateImg(mainLabel);
+//        		currentImgIndex = (currentImgIndex + 1 ) % imagePaths.length;
+//        	    add(textPanel);
+//        	}
+//        	
+        	   });
+        
+        changeImg.start();
+        
+        try {
+        	BufferedImage image = ImageIO.read(new File(imagePaths[currentImgIndex]));
+        	Image scaledImage = image.getScaledInstance(540, 960, Image.SCALE_SMOOTH);
+        	mainLabel.setIcon(new ImageIcon(scaledImage));
+        	
+        } catch (IOException e) {
+        	System.out.println("이미지오류");
+        	e.printStackTrace();
+        }
 
         // 전체 패널에 마우스 클릭 리스너 추가
         mainPanel.addMouseListener(new MouseAdapter() {
@@ -87,6 +108,18 @@ public class IceCreamShopCover extends JFrame {
         setVisible(true);
     }
 
+    public void updateImg(JLabel label) {
+    	 try {
+			BufferedImage image = ImageIO.read(new File(imagePaths[currentImgIndex]));
+			Image scaledImage = image.getScaledInstance(540, 960, Image.SCALE_SMOOTH);
+			label.setIcon(new ImageIcon(scaledImage));
+			
+        } catch (IOException e) {
+        	System.out.println("이미지오류");
+			e.printStackTrace();
+		}
+    }
+    
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new IceCreamShopCover());
     }
