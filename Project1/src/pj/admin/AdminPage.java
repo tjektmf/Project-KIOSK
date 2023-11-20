9415b0844f077a9248113d604dc56e17979146c4
hyemi-bang <pbhm1124@gmail.com>
2023-11-20 16:25:39 +0900

    HM

--- Project1/src/pj/admin/AdminPage.java
+++ Project1/src/pj/admin/AdminPage.java
@@ -1,43 +1,56 @@
 package pj.admin;
 
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
-import java.awt.GridLayout;
-
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class adminPage extends JFrame {
+public class AdminPage extends JFrame {
 
-	//  adminPassword() 에서 비밀번호 입력후 들어오는 창
-	// 예정 항목 : 매출 관리 (손님 주문내역 확인), 상품 관리 (등록,취소,할인,본사에주문,품절관리 등)
-	// 가라페이지
+	//  adminPassword() 에서 비밀번호 입력 후 들어오는 창
+	// 예정 항목 : 매출 관리 (손님 주문내역 확인)
 	
-	public adminPage() {
-		super("IceCreamShop admin");
+	
 		
-		JFrame f = new JFrame();
-		CardLayout card = new CardLayout();
-		f.setLayout(card);
-		
-		JPanel panel1 = new JPanel(new BorderLayout());
-		JPanel panel2 = new JPanel(new GridLayout(3,3));
-		JPanel panel3 = new JPanel(null);
+	public AdminPage() {
+	    super("IceCreamShop admin");
 
-		
-		f.add("1번", panel1);
-		f.add("2번", panel2);
-		f.add("3번", panel3);
-			
-		setSize(540, 960);
+	    JPanel main = new JPanel();
+        main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
+        
+        JPanel row = new JPanel(new FlowLayout(FlowLayout.CENTER, 80, 100));
+	    
+	    JLabel order = new JLabel("주문 내역"); 
+	    customizeLabel(order);
+	    	    
+	
+	    
+	    row.add(order);  
+	       
+        main.add(row);
+        
+        add(main);
+
+	    setSize(540, 960);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBackground(Color.gray);
 		setLocationRelativeTo(null);
 		setVisible(true);
-		
-	}
-	
-	public static void main(String[] args) {
-		new adminPage();
 	}
 
+	private void customizeLabel(JLabel label) {
+        label.setPreferredSize(new Dimension(250, 250));
+        label.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+        label.setBackground(new Color(236, 108, 165));
+        label.setOpaque(true);
+    }
+	
+	public static void main(String[] args) {
+	    new AdminPage();
+	}
+	
 }
