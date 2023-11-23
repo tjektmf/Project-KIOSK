926a52f7f8c78dba9f10f5ee30956ee6264e223a
tweet <tweet@yerin>
2023-11-23 10:05:18 +0900

    테스트

--- Project1/src/pj_yr/Container/Container_04imageSpoon.java
+++ Project1/src/pj_yr/Container/Container_04imageSpoon.java
@@ -4,6 +4,7 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -13,10 +14,10 @@ import javax.swing.JPanel;
 
 public class Container_04imageSpoon {
 
-	public Container_04imageSpoon(JLabel labelName) {
+	public Container_04imageSpoon(JLabel labelName, String route) {
 		try {
 			// 1. 이미지의 크기를 수정하기 위해 불러온다
-			BufferedImage bufferedImage = ImageIO.read(new File("img/daseul/2.png"));
+			BufferedImage bufferedImage = ImageIO.read(new File(route));
 
 			// 2. 불러온 이미지로부터 사이즈 조절된 새로운 인스턴스를 받을 수 있다
 			Image scaledImage = bufferedImage.getScaledInstance(160, 200, Image.SCALE_SMOOTH);
