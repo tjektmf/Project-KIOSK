7aedf7722e8b9246c73226ee43f9b8cba5e047d7
tweet <tweet@yerin>
2023-11-24 14:14:26 +0900

    ConeAndCup 04 매개변수 수정함

--- Project1/src/pj_yr/ChoiceFlavor/ChoiceFlavor_02row2.java
+++ Project1/src/pj_yr/ChoiceFlavor/ChoiceFlavor_02row2.java
@@ -2,6 +2,7 @@ package pj_yr.ChoiceFlavor;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -22,8 +23,6 @@ public class ChoiceFlavor_02row2 extends JPanel {
     private ChoiceFlavor_04showFlavorSelections showFlavorSelections;
     private ChoiceFlavor_03prevOrNext row3; // row3 추가
 
-//    private static final int BUTTONS_NUMS = 16;
-//    private static final int TOTAL_CARDS = 2;
 
     public ChoiceFlavor_02row2(ChoiceFlavor_00frame frame, ChoiceFlavor_04showFlavorSelections showFlavorSelections) {
         this.frame = frame;
@@ -50,6 +49,8 @@ public class ChoiceFlavor_02row2 extends JPanel {
                 cardLayout.previous(cardPanel);
             }
         });
+        prevButton.setBackground(Color.white);
+
         add(prevButton, BorderLayout.WEST);
 
         // >> 
@@ -60,6 +61,8 @@ public class ChoiceFlavor_02row2 extends JPanel {
                 cardLayout.next(cardPanel);
             }
         });
+        nextButton.setBackground(Color.white);
+
         add(nextButton, BorderLayout.EAST);
 
         // 카드 패널 추가 (센터에)
@@ -84,7 +87,8 @@ public class ChoiceFlavor_02row2 extends JPanel {
                         addFlavorSelection(imageIcon);
                     }
                 });
-
+                
+                imageButton.setBackground(Color.white);
                 imageButton.setIcon(imageIcon);
                 cardPanel.add(imageButton);
             } catch (IOException e) {
