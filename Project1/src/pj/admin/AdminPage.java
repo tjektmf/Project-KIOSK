13bee85446be7bf983f1545c5d1ee1ee37b91403
hyemi-bang <145669691+hyemi-bang@users.noreply.github.com>
2023-11-29 18:59:33 +0900

    adminpage 수정

--- Project1/src/pj/admin/AdminPage.java
+++ Project1/src/pj/admin/AdminPage.java
@@ -1,7 +1,7 @@
 package pj.admin;
 
 import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,34 +13,38 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class AdminPage extends JFrame {
+	
+	// 1. 기간 별 주문내역 (일간, 주간, 월간, 년간)
+	// 2. 회원별 포인트 사용내역 (기간별)
+	// 3. 카테고리별 랭킹
+	// 4. 팀원들 이름 넣기, 만든사람들 => 크레딧느낌으로다가
+	// 배라이미지를 모든 버튼에 추가
 
 	public AdminPage() {
 		// 화면구성
-		 setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
-		
-		// 1. 기간 별 주문내역 (일간, 주간, 월간, 년간)
-		// 2. 회원별 포인트 사용내역 (기간별)
-		// 3. 카테고리별 랭킹
-		// 4. 팀원들 이름 넣기, 만든사람들 => 크레딧느낌으로다가 두목 과 졸개 말고 임원진이었으면 
-		// 배라이미지를 모든 버튼에 추가
+		 setLayout(new GridLayout(4,2));
 
         // Button names
         String[] buttonNames = {"OrderPage", "CustomerPage", "RanksPage", "MadeBy"};
+        String[] imgPath = {"img/hyemi/orderpage.png","img/hyemi/customerpage.png","img/hyemi/rankspage.png","img/hyemi/madeby.png"};
+//        JPanel panel1 = new JPanel();
+//        panel1.setSize(150,200);
+//        add(panel1);
 
         for (int i = 0; i < buttonNames.length; ++i) {
             try {
-                BufferedImage originalImage = ImageIO.read(new File("img/hyemi/BRlogo.png"));
+                BufferedImage originalImage = ImageIO.read(new File(imgPath[i]));
 
-                Image scaledImage = originalImage.getScaledInstance(220, 210, Image.SCALE_SMOOTH);
+               // Image scaledImage = originalImage.getScaledInstance(210, 205, Image.SCALE_SMOOTH);
 
-                JButton button = new JButton(new ImageIcon(scaledImage));
-//                button.setPreferredSize(new Dimension(240, 300));
-                button.setBackground(new Color(236, 108, 165));
-                button.setText(buttonNames[i]);
+                JButton button = new JButton(new ImageIcon(originalImage));
+                //button.setText(buttonNames[i]);
                 button.addActionListener(new ButtonListener(buttonNames[i]));
                 add(button);
+                
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -49,6 +53,7 @@ public class AdminPage extends JFrame {
 		setSize(555, 960);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
+		setResizable(false);
 		setVisible(true);
 	}
 
